ac37d47efb3b809597dde4bca7b2613fd81a041e
iyanu456 <oyerindei13@gmail.com>
2024-12-21 03:02:18 +0100

    Default commit message

--- src/components/TabComponent.tsx
+++ src/components/TabComponent.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { House, ArrowLeftRight, Settings } from "lucide-react";
+import { House, ArrowLeftRight, CircleUser } from "lucide-react";
 
 const TabComponent = () => {
   const [activeTab, setActiveTab] = useState<string | null>("home");
@@ -19,10 +19,10 @@ const TabComponent = () => {
 
   return (
     <div className="w-full grid place-items-center mx-auto fixed bottom-0 top-auto left-[auto] right-auto">
-      <div className="bg-[#333333] max-sm:w-[100%] shadow-md sm:flex sm:mb-[2em] max-sm:grid grid-cols-3 md:flex gap-4 px-5 py-2 sm:rounded-xl">
+      <div className="bg-[#333333] max-sm:w-[100%] shadow-md sm:mb-[2.5em] grid grid-cols-3  sm:gap-4 sm:min-w-[30em] sm:p-3 sm:rounded-xl">
         <button
           onClick={() => setActiveTab("home")}
-          className={`grid gap-2 place-items-center py-3 px-4`}
+          className={`sm:rounded-[8px] max-sm:py-3 hover:bg-[#1a1a1a75] grid gap-2 place-items-center py-3 px-4`}
         >
           <House
             height={20}
@@ -36,7 +36,7 @@ const TabComponent = () => {
 
         <button
           onClick={() => setActiveTab("activity")}
-          className={`grid gap-2 place-items-center py-3 px-4`}
+          className={`sm:rounded-[8px] max-sm:py-3 hover:bg-[#1a1a1a75] grid gap-2 place-items-center py-3 px-4`}
         >
           <ArrowLeftRight
             height={20}
@@ -48,19 +48,24 @@ const TabComponent = () => {
           <p className="text-[0.9em]">Activity</p>
         </button>
 
+
         <button
-          onClick={() => setActiveTab("settings")}
-          className={`grid gap-2 place-items-center py-3 px-4`}
+          onClick={() => setActiveTab("profile")}
+          className={`sm:rounded-[8px] max-sm:py-3 hover:bg-[#1a1a1a75] grid gap-2 place-items-center py-3 px-4`}
         >
-          <Settings
+          <CircleUser
             height={20}
             strokeWidth={3}
             width={20}
-            color={`${activeTab === "settings" ? "#007BFF" : "white"}`}
+            color={`${activeTab === "profile" ? "#007BFF" : "white"}`}
           />
 
-          <p className="text-[0.9em]">Settings</p>
+          <p className="text-[0.9em]">Profile</p>
         </button>
+
+        
+
+        
       </div>
       {/*<div className="tab-content">{renderTabContent()}</div>*/}
     </div>
